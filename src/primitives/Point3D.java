f9cf490c1a79918e60bead283419278856ff29e4
sarieldv <sarieldv@gmail.com>
2019-03-26 13:31:46 +0200

    all functions in point3D completed

--- src/primitives/Point3D.java
+++ src/primitives/Point3D.java
@@ -31,7 +31,23 @@ public class Point3D extends Point2D {
 	{
 		_z = z;
 	}
-	
+	public Vector subtract(Point3D p)
+	{
+		return new Vector(new Point3D(_x.subtract(p._x), _y.subtract(_y),_z.subtract(p._z)));
+	}
+	public Point3D add(Vector v)
+	{
+		return new Point3D(_x.add(v.getHead()._x), _y.add(v.getHead()._y), _z.add(v.getHead()._z));
+	}
+	public double squaredDistance(Point3D p)
+	{
+		Point3D ptemp = this.subtract(p).getHead();
+		return ptemp._x.getNum()*ptemp._x.getNum()+ptemp._y.getNum()*ptemp._y.getNum()+ptemp._z.getNum()*ptemp._z.getNum();
+	}
+	public double distance(Point3D p)
+	{
+		return Math.sqrt(this.squaredDistance(p));
+	}
 	@Override
 	public boolean equals(Object p)
 	{
