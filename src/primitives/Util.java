39023574b284647c49568f2d558ac5b57aaa7ddd
sarieldv <sarieldv@gmail.com>
2019-03-28 19:31:35 +0200

    ACCURACY chosen

--- src/primitives/Util.java
+++ src/primitives/Util.java
@@ -2,7 +2,7 @@ package primitives;
 
 public abstract class Util {
 	// It is binary, equivalent to ~1/1,000,000,000,000 in decimal (12 digits)
-		private static final int ACCURACY = -40;
+		private static final double ACCURACY = 0.001;
 
 		// double store format (bit level): seee eeee eeee (1.)mmmm … mmmm
 		// 1 bit sign, 11 bits exponent, 53 bits (52 stored) normalized mantissa
