96610371bc7ce37c1e7e6329eef8dd3fe7e88049
egoldshm <egoldshm@B104-52-78>
2019-04-03 11:26:31 +0300

    Add documentation To camera

--- src/Elements/Camera.java
+++ src/Elements/Camera.java
@@ -1,45 +1,84 @@
 package Elements;
 import primitives.*;
 
+/**
+ * @author egoldshm 
+ * @info 
+ * Object to represent a camera in 3D space
+ */
 public class Camera {
 	private Point3D _PO;
 	private Vector _vUp;
 	private Vector _vTo;
 	private Vector _vRight;
 	
+	/**
+	 * @return PO point, where the camera is positioned in space
+	 */
 	public Point3D getPO()
 	{
 		return _PO;
 	}
+	/**
+	 * @param p PO point, where the camera is positioned in space
+	 */
 	public void setPO(Point3D p)
 	{
 		_PO=p;
 	}
+	
+	/**
+	 * @return Vector direction the top of the camera
+	 */
 	public Vector getVUp()
 	{
 		return _vUp;
 	}
+	/**
+	 * @param v Vector direction the top of the camera
+	 */
 	public void setVUp(Vector v)
 	{
 		_vUp=v;
 	}
+	/**
+	 * @return Direct vector direction of the camera
+	 */
 	public Vector getVTo()
 	{
 		return _vTo;
 	}
+	
+	/**
+	 * @param v Direct vector direction of the camera
+	 */
 	public void setVTo(Vector v)
 	{
 		_vTo=v;
 	}
+	
+	/**
+	 * @return Vector orientation to the sides of the camera
+	 */
 	public Vector getVRight()
 	{
 		return _vRight;
 	}
+	/**
+	 * @param v Vector orientation to the sides of the camera
+	 */
 	public void setVRight(Vector v)
 	{
 		_vRight=v;
 	}
 	
+	/**
+	 * Constructor with parameters
+	 * @param PO point, where the camera is positioned in space
+	 * @param vUp Vector direction the top of the camera
+	 * @param vTo Direct vector direction of the camera
+	 * @param vRight Vector orientation to the sides of the camera
+	 */
 	public Camera(Point3D PO, Vector vUp, Vector vTo, Vector vRight)
 	{
 		_PO=PO;
@@ -47,6 +86,9 @@ public class Camera {
 		_vTo=vTo;
 		_vRight=vRight;
 	}
+	/**
+	 * copy constructor
+	 */
 	public Camera(Camera c)
 	{
 		
@@ -55,6 +97,9 @@ public class Camera {
 		_vTo=c.getVTo();
 		_vRight=c.getVRight();
 	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
 	public boolean equals(Object c)
 	{
@@ -69,12 +114,25 @@ public class Camera {
 	        return false;
   	   return _PO.equals(((Camera)c).getPO())&&_vUp.equals(((Camera)c).getVUp())&&_vTo.equals(((Camera)c).getVTo())&&_vRight.equals(((Camera)c).getVRight());
 	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString()
 	{
 		return _PO.toString()+" "+_vUp.toString()+" "+_vTo.toString()+" "+_vRight.toString();
 	}
 	
+	/**
+	 * Returns the Ray from the camera passing through a certain point
+	 * @param Nx
+	 * @param Ny
+	 * @param x
+	 * @param y
+	 * @param screenDistance
+	 * @param screenWidth
+	 * @param screenHeight
+	 */
 	public Ray constructRayThroughPixel(int Nx, int Ny, double x, double y, double screenDistance, double screenWidth, double screenHeight)
 	{
 		Point3D Pc = _PO.add(_vTo.scalarMultiplication(screenDistance));
