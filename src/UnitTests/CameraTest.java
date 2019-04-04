e1b566f107b98060b8c135d33f9d96d329edb9c9
sarieldv <sarieldv@gmail.com>
2019-04-04 19:37:38 +0300

    Camera test finished

--- src/UnitTests/CameraTest.java
+++ src/UnitTests/CameraTest.java
@@ -15,9 +15,9 @@ public class CameraTest
 	@Test
 	public void testConstructRayThroughPixel()
 	{
-		Camera testCamera = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(1, 0, 0), new Vector(0, 1, 0));
-		int Nx = 5, Ny = 5;
-		double x = 3, y = 3, screenDistance = 1, screenWidth = 5, screenHeight = 5;
-		assertEquals(testCamera.constructRayThroughPixel(Nx, Ny, x, y, screenDistance, screenWidth, screenHeight), new Ray(Point3D.ZERO, new Vector(1, 0, 0)));
+		Camera testCamera = new Camera(Point3D.ZERO, new Vector(0, 1, 0), new Vector(0, 0, -1), new Vector(1, 0, 0));
+		int Nx = 3, Ny = 3;
+		double x = 3, y = 3, screenDistance = 100, screenWidth = 150, screenHeight = 150;
+		assertEquals(testCamera.constructRayThroughPixel(Nx, Ny, x, y, screenDistance, screenWidth, screenHeight), new Ray(Point3D.ZERO, new Vector(50, -50, -100).normalizationOfVector()));
 	}
 }
