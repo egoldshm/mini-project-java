a3669a855797f44c8525300e196a1719117f8618
egoldshm <egoldshm@B104-52-78>
2019-05-01 22:30:23 +0300

    fixed bug

--- src/UnitTests/PlaneTest.java
+++ src/UnitTests/PlaneTest.java
@@ -17,7 +17,7 @@ public class PlaneTest {
 	@Test
 	public void testRayIntersections() {
 		Plane testPlane = new Plane(new Vector(12,-4,1),new Point3D(0,0,4));
-		Point3D pTest =new Point3D(1.29,4.43,6.29);
+		Point3D pTest =new Point3D(1.2857,4.4287,6.2857);
 		Ray testRay= new Ray(new Point3D(1,1,4),new Vector(-1,-12,-8));	
 		assertEquals(true,(testPlane.findIntersections(testRay)).get(0).equals(pTest));
 				
